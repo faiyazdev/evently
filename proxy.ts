d6f4c0ea469c88d3bb166df0be43367c206187e1
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-06-18 23:05:12 +0600

    feat(auth): initialize Clerk auth, middleware, and custom components
    - Install @clerk/nextjs dependency
    - Configure clerkMiddleware to enforce route protection with public and auth matchers
    - Create custom sign-in and sign-up routing components

---
+++ proxy.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/events",
+  "/events/:id",
+  "/api/wehhook(.*)",
+]);
+const isAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { isAuthenticated, redirectToSignIn } = await auth();
+  if (!isPublicRoute(req) && !isAuthRoute(req)) {
+    redirectToSignIn();
+  }
+
+  if (isAuthenticated && isAuthRoute(req)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+    // Always run for Clerk-specific frontend API routes
+    "/__clerk/(.*)",
+  ],
+};
