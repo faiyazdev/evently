edc940c6f533679f115dc6ecbdb9299edd6907a0
Faiyaz Sarkar02 <faiyazsarkar24@gmail.com>
2026-07-05 21:47:43 +0600

    fix: update route matching for public and authenticated routes in clerkMiddleware

--- proxy.ts
+++ proxy.ts
@@ -3,16 +3,18 @@ import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
-  "/events",
-  "/events/:id",
-  "/api/wehhook(.*)",
-  "/api/webhook/clerk",
+  "/events(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhook(.*)",
 ]);
+
 const isAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { isAuthenticated, redirectToSignIn } = await auth();
-  if (isAuthenticated === false && !isPublicRoute(req)) {
+
+  if (!isAuthenticated && !isPublicRoute(req)) {
     return redirectToSignIn();
   }
 
@@ -20,14 +22,3 @@ export default clerkMiddleware(async (auth, req) => {
     return NextResponse.redirect(new URL("/", req.url));
   }
 });
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-    // Always run for Clerk-specific frontend API routes
-    "/__clerk/(.*)",
-  ],
-};
