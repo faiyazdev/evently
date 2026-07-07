3196493ef38c09fe5792f059489674cc6126f6b5
Faiyaz Sarkar02 <faiyazsarkar24@gmail.com>
2026-07-07 12:33:07 +0600

    fix: simplify authentication logic in clerkMiddleware

--- proxy.ts
+++ proxy.ts
@@ -1,22 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/events(.*)",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhook(.*)",
-]);
+// const isPublicRoute = createRouteMatcher([
+//   "/",
+//   "/events(.*)",
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+//   "/api/webhook(.*)",
+// ]);
 
 const isAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { isAuthenticated, redirectToSignIn } = await auth();
-
-  if (!isAuthenticated && !isPublicRoute(req)) {
-    return redirectToSignIn();
-  }
+  const { isAuthenticated } = await auth();
 
   if (isAuthenticated && isAuthRoute(req)) {
     return NextResponse.redirect(new URL("/", req.url));
