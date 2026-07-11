55a20d6d59600da272c19356fe16f01d29fab4c8
Faiyaz Sarkar02 <faiyazsarkar24@gmail.com>
2026-07-11 11:15:57 +0600

    feat(categories): cache category data fetching

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -4,6 +4,7 @@ import { CreateCategoryParams } from "@/types";
 import connectToDatabase from "../db";
 import { CategoryDto, toCategoryDto } from "../dto/category.dto";
 import Category from "../db/models/category.model";
+import { unstable_cache } from "next/cache";
 
 export const createCategory = async ({
   categoryName,
@@ -18,15 +19,22 @@ export const createCategory = async ({
   }
 };
 
-export const getCategories = async (): Promise<CategoryDto[] | null> => {
-  try {
-    await connectToDatabase();
-    console.log("categories rendered");
-    const categories = await Category.find().lean();
-    if (!categories) return null;
-    return categories.map(toCategoryDto);
-  } catch (error) {
-    console.error("Error fetching Categories:", error);
-    throw error;
-  }
-};
+export const getCategories = unstable_cache(
+  async (): Promise<CategoryDto[] | null> => {
+    try {
+      await connectToDatabase();
+      console.count("categories rendered");
+      const categories = await Category.find().lean();
+      if (!categories) return null;
+      return categories.map(toCategoryDto);
+    } catch (error) {
+      console.error("Error fetching Categories:", error);
+      throw error;
+    }
+  },
+  ["categories"],
+  {
+    tags: ["categories"],
+    revalidate: 3600,
+  },
+);
